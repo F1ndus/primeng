3424c7bfd6f2006cfaccc0f7fc56f202ada7137f
Philipp Hentschel <philipp.hentschel@msg-david.de>
2018-12-17 16:11:06 +0100

    Started implementing more responsive approach

--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -36,9 +36,7 @@ import {CommonModule} from '@angular/common';
     `,
     providers: [DomHandler]
 })
-export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
-    
-    @Input() numVisible: number = 3;
+export class Carousel implements AfterViewChecked, AfterViewInit, OnDestroy {
 
     @Input() firstVisible: number = 0;
 
@@ -51,9 +49,9 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
     @Input() responsive: boolean = true;
 
     @Input() autoplayInterval: number = 0;
-    
+
     @Input() effectDuration: any = '1s';
-        
+
     @Input() easing: string = 'ease-out';
 
     @Input() pageLinks: number = 3;
@@ -61,61 +59,72 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
     @Input() style: any;
 
     @Input() styleClass: string;
-    
+
     @Output() onPage: EventEmitter<any> = new EventEmitter();
-        
+
     @ContentChildren(PrimeTemplate) templates: QueryList<any>;
-    
+
     public _value: any[];
-    
+
     public itemTemplate: TemplateRef<any>;
-            
+
     public left: any = 0;
-        
+
     public items: any;
-    
+
     public columns: number = 0;
-        
+
     public page: number;
-                    
+
     public valuesChanged: any;
-    
+
     public interval: any;
-    
+
     public anchorPageLinks: any[];
-    
+
     public mobileDropdownOptions: any[];
-    
+
     public selectDropdownOptions: any[];
-    
+
     public shrinked: boolean;
-    
+
     @ViewChild('container') containerViewChild: ElementRef;
-    
+
     @ViewChild('viewport') viewportViewChild: ElementRef;
-        
+
     @ViewChild('items') itemsViewChild: ElementRef;
-    
+
     documentResponsiveListener: any;
-    
+
     differ: any;
+    private _nums: number = 4 ;
+    private MIN_COMPONENT_WIDTH = 200;
 
     constructor(public el: ElementRef, public domHandler: DomHandler, public renderer: Renderer2, public cd: ChangeDetectorRef) {}
-    
+
     ngAfterContentInit() {
         this.templates.forEach((item) => {
             switch(item.getType()) {
                 case 'item':
                     this.itemTemplate = item.template;
                 break;
-                
+
                 default:
                     this.itemTemplate = item.template;
                 break;
             }
         });
     }
-    
+
+    @Input() get numVisible(): number {
+        return this._nums;
+    }
+
+    set numVisible(val: number) {
+        this._nums = val;
+        this.handleDataChange();
+    }
+
     @Input() get value(): any[] {
         return this._value;
     }
@@ -124,7 +133,7 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
         this._value = val;
         this.handleDataChange();
     }
-    
+
     handleDataChange() {
         if(this.value && this.value.length) {
             if(this.value.length && this.firstVisible >= this.value.length) {
@@ -137,14 +146,15 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
 
         this.valuesChanged = true;
     }
-        
+
     ngAfterViewChecked() {
-        if(this.valuesChanged && this.containerViewChild.nativeElement.offsetParent) {
+        if (this.valuesChanged && this.containerViewChild.nativeElement.offsetParent) {
+            this.updateState();
             this.render();
             this.valuesChanged = false;
         }
     }
-    
+
     ngAfterViewInit() {
         if(this.responsive) {
             this.documentResponsiveListener = this.renderer.listen('window', 'resize', (event) => {
@@ -152,63 +162,71 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
             });
         }
     }
-    
+
     updateLinks() {
         this.anchorPageLinks = [];
         for (let i = 0; i < this.totalPages; i++) {
             this.anchorPageLinks.push(i);
         }
     }
-    
+
     updateDropdown() {
         this.selectDropdownOptions = [];
         for (let i = 0; i < this.totalPages; i++) {
             this.selectDropdownOptions.push(i);
         }
     }
-    
+
     updateMobileDropdown() {
         this.mobileDropdownOptions = [];
         if(this.value && this.value.length) {
             for (let i = 0; i < this.value.length; i++) {
                 this.mobileDropdownOptions.push(i);
             }
-        } 
+        }
     }
-    
+
     render() {
         if(this.autoplayInterval) {
             this.stopAutoplay();
         }
-        
+
         this.items = this.domHandler.find(this.itemsViewChild.nativeElement, 'li');
         this.calculateColumns();
         this.calculateItemWidths();
-        
+
         if(!this.responsive) {
             this.containerViewChild.nativeElement.style.width = (this.domHandler.width(this.containerViewChild.nativeElement)) + 'px';
         }
-        
+
         if(this.autoplayInterval) {
             this.circular = true;
             this.startAutoplay();
         }
-        
+
         this.updateMobileDropdown();
         this.updateLinks();
         this.updateDropdown();
         this.cd.detectChanges();
     }
-    
+
     calculateItemWidths () {
         let firstItem = (this.items && this.items.length) ? this.items[0] : null;
         if(firstItem) {
             for (let i = 0; i < this.items.length; i++) {
-                this.items[i].style.width = ((this.domHandler.innerWidth(this.viewportViewChild.nativeElement) - (this.domHandler.getHorizontalMargin(firstItem) * this.columns)) / this.columns) + 'px';
+                this.items[i].style.width = ( this.getComponentWidth() / this.columns + 'px');
             }
         }
     }
-    
+
+    getComponentWidth () {
+        const firstItem = (this.items && this.items.length) ? this.items[0] : null;
+        if ( firstItem ) {
+            return this.domHandler.innerWidth(this.viewportViewChild.nativeElement) -
+                (this.domHandler.getHorizontalMargin(firstItem) * this.columns);
+        }
+    }
+
     calculateColumns() {
         if(window.innerWidth <= this.breakpoint) {
             this.shrinked = true;
@@ -220,7 +238,7 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
         }
         this.page = Math.floor(this.firstVisible / this.columns);
     }
-    
+
     onNextNav() {
         let lastPage = (this.page === (this.totalPages - 1));
 
@@ -229,19 +247,19 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
         else if(this.circular)
             this.setPage(0);
     }
-    
+
     onPrevNav() {
         if(this.page !== 0)
             this.setPage(this.page - 1);
         else if(this.circular)
             this.setPage(this.totalPages - 1);
     }
-    
+
     setPageWithLink(event, p: number) {
         this.setPage(p);
         event.preventDefault();
     }
-    
+
     setPage(p, enforce?: boolean) {
         if(p !== this.page || enforce) {
             this.page = p;
@@ -252,23 +270,23 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
             });
         }
     }
-    
+
     onDropdownChange(val: string) {
         this.setPage(parseInt(val));
     }
-    
+
     get displayPageLinks(): boolean {
         return (this.totalPages <= this.pageLinks && !this.shrinked);
     }
-    
+
     get displayPageDropdown(): boolean {
         return (this.totalPages > this.pageLinks && !this.shrinked);
     }
-    
+
     get totalPages(): number {
         return (this.value && this.value.length) ? Math.ceil(this.value.length / this.columns) : 0;
     }
-        
+
     routerDisplay () {
         let win = window;
         if(win.innerWidth <= this.breakpoint)
@@ -276,43 +294,59 @@ export class Carousel implements AfterViewChecked,AfterViewInit,OnDestroy{
         else
             return false;
     }
-    
+
     updateState() {
-        let win = window;
-        if(win.innerWidth <= this.breakpoint) {
-            this.shrinked = true;
-            this.columns = 1;
-        }
-        else if(this.shrinked) {
-            this.shrinked = false;
-            this.columns = this.numVisible;
-            this.updateLinks();
-            this.updateDropdown();
-        }
-        
+        this.recalc();
         this.calculateItemWidths();
         this.setPage(Math.floor(this.firstVisible / this.columns), true);
+        this.updateLinks();
+        this.updateDropdown();
     }
-    
+
+    recalc() {
+        const oldValue = this._nums;
+        const componentwidth = this.getComponentWidth();
+        const items = this.items;
+        if ( this._value.length > 0) {
+            // @ts-ignore
+            const fittingItems = parseInt(componentwidth / this.MIN_COMPONENT_WIDTH , 10);
+            if ( fittingItems > this.value.length  && this.value.length > 0) {
+                this.columns = this._nums;
+            } else {
+                if (fittingItems > 0) {
+                    if ( fittingItems > this._nums ) {
+                        this.columns = this._nums;
+                    } else {
+                        this.columns = fittingItems;
+                    }
+                } else {
+                    this.columns = 1;
+                }
+            }
+        } else {
+            this.columns = 1;
+        }
+    }
+
     startAutoplay() {
         this.interval = setInterval(() => {
             if(this.page === (this.totalPages - 1))
                 this.setPage(0);
             else
                 this.setPage(this.page + 1);
-        }, 
+        },
         this.autoplayInterval);
     }
 
     stopAutoplay() {
         clearInterval(this.interval);
     }
-    
+
     ngOnDestroy() {
         if(this.documentResponsiveListener) {
             this.documentResponsiveListener();
         }
-        
+
         if(this.autoplayInterval) {
             this.stopAutoplay();
         }
